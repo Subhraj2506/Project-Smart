56c17b6140e5d990028af95f27dea8cdefa49d04
Subhraj2506 <rajsubh2003@gmail.com>
2025-11-20 08:36:22 +0530

    Update AuthService.java

--- AuthService.java
+++ AuthService.java
@@ -1,1 +1,6 @@
-package com.egov.service; import com.egov.dto.*; public interface AuthService { ApiResponse register(RegisterRequest request); AuthResponse login(LoginRequest request); }
+package com.egov.service;
+import com.egov.dto.*;
+public interface AuthService {
+  ApiResponse register(RegisterRequest request);
+  AuthResponse login(LoginRequest request);
+}
